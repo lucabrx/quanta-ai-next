79969c013cffe31ad4df3014c1da8a3c2c67ecea
Luka Brkovic <lukabrkovic@proton.me>
2023-07-23 00:13:54 +0200

    very nice 21312 mains

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -8,10 +8,10 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
       <div className="hidden h-full md:flex md:flex-col md:w-72 md:fixed md:inset-y-0 z-[60] bg-gray-900">
         <Sidebar />
       </div>
-      <main className="md:pl-72">
+      <div className="md:pl-72">
         <Navbar />
         {children}
-      </main>
+      </div>
     </div>
   )
 }
