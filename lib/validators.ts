1bbfbc023e380ed5e1ac103ee3a2ac8b83609096
Luka Brkovic <lukabrkovic@proton.me>
2023-07-23 22:58:36 +0200

    .

--- lib/validators.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod'
-
-export const promptValidator = z.object({
-  prompt: z.string().min(1, 'Prompt is required.'),
-})
-export type PromptValidator = z.infer<typeof promptValidator>
