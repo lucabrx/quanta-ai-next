7fe915ec56418c8fddc1dde131da117c5fb62ba1
Luka Brkovic <lukabrkovic@proton.me>
2023-07-31 07:29:28 +0200

    test home loading

---
+++ app/(home)/loading.tsx
@@ -0,0 +1,18 @@
+export default function LoadingPage() {
+	return (
+		<main className="flex flex-col ">
+			<div className="flex  items-center justify-between">
+				<h2 className="text-xl md:text-2xl">Loading...</h2>
+			</div>
+
+			<div className="pt-8 md:pt-24 mx-auto flex justify-center items-center">
+				<div className="relative overflow-hidden shadow rounded-lg border bg-background p-2">
+					<div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-primary/30 to-secondary/30 opacity-10"></div>
+					<div className="flex h-[120px] flex-col justify-between rounded-md p-6">
+						<div className="w-4 h-4 animate-spin" />
+					</div>
+				</div>
+			</div>
+		</main>
+	)
+}
